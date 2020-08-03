053756d09ffc903f365ac5209d50a8b7626d3fae
Anthony Chen <anthonychen33@gmail.com>
2020-08-02 20:05:11 -0400

    Finished the default page. Work on validation and getting data.

--- index.php
+++ index.php
@@ -13,30 +13,123 @@
   <body>
 
     <div id = "container"> 
-      <h1> Secret Dairy </h1> 
+      <h1> Secret Dairy </h1> <br>
 
       <p id = "both_modes"> Store your thoughts permenantly and securely. </p> 
 
     <div> 
-      <p id = "signUp_text"> Interested? Sign up now. </p>  
+      <p id = "signUp_text">      Interested? Sign up now. </p>  <br>
 
+      <!-- START: signUp form  -->
       <div id = "signUp_container"> 
 
         <form method = post> 
           <input type = "text" name = "signUp_email" placeholder = "Email" id = "email"> 
+          <input type = "text" name = "login_email" placeholder = "Email" id = "login_email"> <br>
 
-          <input type = "password" name = "signUp_password" placeholder = "Password" id = "password"> 
+          <input type = "password" name = "signUp_password" placeholder = "Password" id = "password">
+          <input type = "password" nmame = "login_password" placeholder = "Password" id = "login_password"> 
+           <br>
+          
 
           <div id = "checkbox_container">
-            <label for = "stayLoggedIn"> Stay Logged In </label>
+            <label for = "stayLoggedIn"> Stay Logged In </label> 
             <input type = "checkbox" name = "stayLoggedIn" value = "1" id = "stayLoggedIn">
           </div>
+
+          <br> 
+
           <input type = "submit" name = "submit" value = "Sign Up" id = "signUp_button">
+          <input type = "submit" name = "login_submit" value = "Login" id = "login_button">
 
         </form> 
 
       </div>
+    <!-- END: SignUp form -->
+
+
+      <div id = "switch_modes"> 
+        Login
+      </div>
+      
+
+
     <!-- Optional JavaScript -->
+    <script type = "text/javascript"> 
+
+         // var switch = document.getElementById("switch_modes");
+
+         var mode = 0; // 0 = Sign Up : 1 = Login
+
+          document.getElementById("switch_modes").onclick = function()  {
+            myFunction()
+          };
+          
+          // Function to change the page. 
+          function myFunction() {
+
+            if ( mode == 0 )  {
+              document.getElementById("switch_modes").innerHTML = "     Login"; 
+              document.getElementById("signUp_text").innerHTML = "  Log in using your email and password.";
+              
+              // START: Make the login email and password visible 
+              document.getElementById("login_email").style.display = "block";
+              document.getElementById("login_email").style.visibility = "visible";
+
+              document.getElementById("login_password").style.display = "block";
+              document.getElementById("login_password").style.visibility = "visible";
+              // END: Login email and password visible 
+
+              // START: Signup Email and password invisible
+              document.getElementById("email").style.display = "none";  
+              document.getElementById("email").style.visibility = "hidden"; 
+
+              document.getElementById("password").style.display = "none"; 
+              document.getElementById("password").style.visibility = "hidden"; 
+              // END: SignUp email and password invisible 
+              
+              document.getElementById("signUp_button").style.display = "none";
+              document.getElementById("signUp_button").style.visibility = "hidden";
+
+              document.getElementById("login_button").style.display = "block";
+              document.getElementById("login_button").style.visibility = "visible";
+
+
+              mode = 1;
+            } else  {
+              document.getElementById("switch_modes").innerHTML = "Sign Up"; 
+              document.getElementById("signUp_text").innerHTML = "Interested? Sign up now.";
+
+              // START: Make the login email and password visible 
+              document.getElementById("login_email").style.display = "none";
+              document.getElementById("login_email").style.visibility = "hidden";
+
+              document.getElementById("login_password").style.display = "none";
+              document.getElementById("login_password").style.visibility = "hidden";
+              // END: Login email and password visible 
+
+              // START: Signup Email and password invisible
+              document.getElementById("email").style.display = "block";  
+              document.getElementById("email").style.visibility = "visible"; 
+
+              document.getElementById("password").style.display = "block"; 
+              document.getElementById("password").style.visibility = "visible"; 
+              // END: SignUp email and password invisible 
+
+              document.getElementById("signUp_button").style.display = "block";
+              document.getElementById("signUp_button").style.visibility = "visible";
+
+              document.getElementById("login_button").style.display = "none";
+              document.getElementById("login_button").style.visibility = "hidden";
+
+           //   document.getElementById("signUp_button").value = "Login";
+
+              mode = 0; 
+            }
+          }
+
+
+    </script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
